9737a6891728708bf50fc0bc0c5dc01fd38a5505
Alex Clark <aclark@aclark.net>
2012-12-08 16:47:48 -0500

    Spec out the rest

--- other.py
+++ other.py
@@ -11,33 +11,10 @@ from zope.interface import invariant
 from zope.interface.adapter import AdapterRegistry
 
 
-#The Zen of Python, by Tim Peters
-#
-#Beautiful is better than ugly.
-#Explicit is better than implicit.
-#Simple is better than complex.
-#Complex is better than complicated.
-#Flat is better than nested.
-#Sparse is better than dense.
-#Readability counts.
-#Special cases aren't special enough to break the rules.
-#Although practicality beats purity.
-#Errors should never pass silently.
-#Unless explicitly silenced.
-#In the face of ambiguity, refuse the temptation to guess.
-#There should be one-- and preferably only one --obvious way to do it.
-#Although that way may not be obvious at first unless you're Dutch.
-#Now is better than never.
-#Although never is often better than *right* now.
-#If the implementation is hard to explain, it's a bad idea.
-#If the implementation is easy to explain, it may be a good idea.
-#Namespaces are one honking great idea -- let's do more of those!
-
-
 class IUgly(Interface):
     """
     """
-    beautiful = Attribute("Beautiful is an attribute of ugly")
+    beautiful = Attribute("Beautiful is an attribute of ugly.")
 
 
 class IExplicit(Interface):
@@ -182,69 +159,88 @@ class Explicit:
 
 
 print "The Zen of Zope, by Alex Clark\n\n"
-# 1)
+
+
+# 1) Beautiful is better than ugly.
 if 'beautiful' in IUgly:
     print IUgly['beautiful'].__doc__
 
-# 2)
+# 2) Explicit is better than implicit.
 if IExplicit.implementedBy(Implicit):
-    print "Explicit is implemented by implicit"
+    print "Explicit is implemented by implicit."
 
-# 3)
+# 3) Simple is better than complex.
 simple = Complex()
 if IComplex.providedBy(simple):
-    print "Simple is provided by complex"
+    print "Simple is provided by complex."
 
-# 4)
+# 4) Complex is better than complicated.
 complicated = Complicated()
 directlyProvides(complicated, IComplex)
 for interface in directlyProvidedBy(complicated).interfaces():
     if interface.getName() == 'IComplex':
-        print "Complex is directly provided by complicated"
+        print "Complex is directly provided by complicated."
 
-# 5)
+# 5) Flat is better than nested.
 interfaces = [interface for interface in implementedBy(Flat).interfaces()]
 if len(interfaces) == 1:
     if interfaces[0].getName() == 'INested':
-        print "Flat only implements nested"
+        print "Flat only implements nested."
 
-# 6)
-ISparse.setTaggedValue('dense', 'Sparse has tagged value dense')
+# 6) Sparse is better than dense.
+ISparse.setTaggedValue('dense', 'Sparse has tagged value dense.')
 tags = ISparse.getTaggedValueTags()
 if 'dense' in tags:
     print ISparse.getTaggedValue('dense')
 
 
-# 7)
+# 7) Readability counts.
 try:
     IRange.validateInvariants(Range(2, 1))
 except RangeError:
-    print "Readability count is not in range"
+    print "Readability count is not in range."
 
-# 8)
+# 8) Special cases aren't special enough to break the rules.
 try:
     ISpecialCases('the rules')
 except TypeError:
-    print "Special cases could not adapt the rules"
+    print "Special cases could not adapt the rules."
 
-# 9)
+# 9) Although practicality beats purity.
 practicality = Practicality()
 if IPurity(practicality) is practicality:
-    print "Practicality implements purity"
+    print "Practicality implements purity."
 
-# 10) Register an object that depends on IErrors and provides ISilence
+# 10) Errors should never pass silently.
+# Register an object that depends on IErrors and provides ISilence
 error_registry = AdapterRegistry()  # XXX Logic below is not quite right
 error_registry.register([IErrors], ISilence, 'should not', 'pass')
 if (error_registry.lookup([IErrors], ISilence, 'should not') == 'pass' and
     error_registry.lookup([Interface], ISilence) is None):
     print ("Errors should never require a specification that doesn’t extend "
-        "the specification of silence")
+        "the specification of silence.")
 
-# 11)
+# 11) Unless explicitly silenced.
 errors = Errors()
 silence = Silence()
 error_registry.register([IErrors, ISilence], IPass, '', Explicit)
 explicit = error_registry.queryMultiAdapter((errors, silence), IPass)
 if (explicit.__class__.__name__ == "Explicit" and
     explicit.errors is errors and explicit.silence is silence):
-    print "Unless explicit is a multi-adapter"
+    print "Unless explicit is a multi-adapter."
+
+# 12) In the face of ambiguity, refuse the temptation to guess.
+
+# 13) There should be one-- and preferably only one --obvious way to do it.
+
+# 14) Although that way may not be obvious at first unless you're Dutch.
+
+# 15) Now is better than never.
+
+# 16) Although never is often better than *right* now.
+
+# 17) If the implementation is hard to explain, it's a bad idea.
+
+# 18) If the implementation is easy to explain, it may be a good idea.
+
+# 19) Namespaces are one honking great idea -- let's do more of those!
