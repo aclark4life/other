fdc678c6a9326778981515759b3c0741c6e6faeb
Alex Clark <aclark@aclark.net>
2012-12-08 22:17:21 -0500

    Add readme

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ VERSION = '1.0.0'
 
 
 setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    description='The Zen of Zope, by Alex Clark',
+    long_description=open('README.rst').read(),
     install_requires=[
+        # This: included in the stdlib
+        'that',  # That: included for fun
         'zope.component',
-#        'this',  # This. Included in stdlib
-        'that',  # That. Included for reference
     ],
-    name='other',  # The other! Get it?
+    name='other',  # The other: get it?!
     py_modules=[
         'other',  
     ],
